3b638339e497e564fce28110a28210fd92625fcf
AnushkaPawar23 <anushkapawar266@gmail.com>
2026-04-06 22:51:33 +0530

    Update auth.js

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8081/api';
+const API_BASE_URL = 'https://medsecure-hwfv.onrender.com';
 
 function checkAuth() {
     const user = localStorage.getItem('medsecure_user');
